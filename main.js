3a0426ac28c531d3ec678c4cfc0f8e51f2af1890
Terts Diepraam <terts.diepraam@gmail.com>
2025-02-06 21:43:28 +0100

    typewriter animatie

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ const messages = document.getElementById("messages");
 const prompt_input = document.getElementById("prompt");
 const send_button = document.getElementById("send");
 
+prompt_input.focus();
+
 let introduction = "";
-let responses = []; 
+let responses = [];
 let ready_for_input = false;
+let rest_of_response = "";
 
 fetch("./answers.txt").then((r) => r.text()).then((r) => {
     const lines = r.split("\n\n");
@@ -16,10 +19,32 @@ fetch("./answers.txt").then((r) => r.text()).then((r) => {
     add_message("bot", introduction);
 })
 
+const reveal_text = (element, str, idx) => {
+    element.innerText = str.substring(0, idx + 1);
+    let delay;
+    const c = str.charAt(idx);
+    if (c === "." || c === "!" || c === "?") {
+        delay = 200;
+    } else if (c === ",") {
+        delay = 150;
+    } else {
+        delay = Math.floor(Math.random() * 20);
+    }
+    if (idx + 1 < str.length) {
+        setTimeout(() => reveal_text(element, str, idx + 1), delay)
+    } else {
+        ready_for_input = true;
+    }
+}
 
 const add_message = (cls, msg) => {
     const p = document.createElement("p");
-    p.innerText = msg
+
+    if (cls === "bot") {
+        reveal_text(p, msg, 0);
+    } else {
+        p.innerText = msg;
+    }
 
     const message_text = document.createElement("div");
     message_text.className = "message-text";
@@ -30,7 +55,7 @@ const add_message = (cls, msg) => {
     message.classList.add(cls);
     message.appendChild(message_text);
 
-    messages.appendChild(message)
+    messages.appendChild(message);
 
     messages.scrollTo(0, messages.scrollHeight);
 }
@@ -50,6 +75,10 @@ const send_message = () => {
         return;
     }
 
+    if (prompt_input.value.trim() === "") {
+        return;
+    }
+
     add_message("human", prompt_input.value);
     prompt_input.value = "";
     ready_for_input = false;
@@ -65,5 +94,3 @@ prompt_input.addEventListener("keypress", (evt) => {
 send_button.addEventListener("click", () => {
     send_message();
 })
-
-// add_message("human", "booooom!");
